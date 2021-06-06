6cab03cf764a12e951e795635adac2a27d5fc80e
Nazli Balatlioglu <nbalatli@ford.com.tr>
2021-06-06 23:40:04 +0300

    travels

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,41 +1,104 @@
-function handleSubmit(event) {
-    event.preventDefault()
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    console.log(formText);
-    if (Client.checkForUrl(formText)) {
-        postData('http://localhost:8081/api', { url: formText })
-            .then(function (res) {
-                document.getElementById('polarity').innerHTML = `Polarity: ${res.score_tag}`;
-                document.getElementById("agreement").innerHTML = `Agreement: ${res.agreement}`;
-                document.getElementById("subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
-                document.getElementById("confidence").innerHTML = `Confidence: ${res.confidence}`;
-                document.getElementById("irony").innerHTML = `Irony: ${res.irony}`;
-            })
-    } else {
-        alert('Invalid URL :(');
-    }
-}
-// Async POST
-const postData = async (url = '', data = {}) => {
-    const response = await fetch(url, {
+const handleSubmit = async event => {
+    event.preventDefault();
+    const cityName = document.getElementById('city').value;
+    const startDate = document.getElementById('start-date').value;
+    const endDate = document.getElementById('end-date').value;
+    let geoLocation = {};
+
+    console.log('Fetching geographical data from geonames:', {
+        city: cityName,
+    });
+
+    const resGeo = await fetch('http://localhost:8081/geo', {
         method: 'POST',
         credentials: 'same-origin',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify({ city: cityName }),
     });
     try {
-        console.log("I am here");
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
+        const receivedGeoInJson = await resGeo.json();
+        geoLocation = {
+            city: receivedGeoInJson.geonames[0].name,
+            country: receivedGeoInJson.geonames[0].countryName,
+            lat: receivedGeoInJson.geonames[0].lat,
+            lng: receivedGeoInJson.geonames[0].lng,
+        };
+        console.log('GeoData:', geoLocation);
+    } catch (err) {
+        console.log(err);
     }
-    catch (error) {
-        console.log("error", error);
-    }
-};
 
+    let weatherData = {};
+
+    const resWeather = await fetch('http://localhost:8081/weather', {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(geoLocation),
+    });
+    try {
+        weatherData = await resWeather.json();
+        console.log('WeatherData:', weatherData);
+    } catch (err) {
+        console.log(err);
+    }
+    let picData = {};
+    const resPic = await fetch('http://localhost:8081/picture', {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cityLatLon),
+    }).then(res => res.json())
+        .then(async res => {
+            
+        console.log('PicData:', { cityLatLongeoLocation });
+            picData = res;
+            let pictureHtml = `<div class="city-image">
+                                       <img id="city-img" class="main-image" src="${picData.hits[0].webformatURL}">
+                                    </div>`;
+            let subpic = `<div class= "text-center">
+                                         <img id="img-one" class="sub-image" src="${picData.hits[1].webformatURL}"/>
+                                         <img id="img-two" class="sub-image" src="${picData.hits[2].webformatURL}"/>
+                                        <img id="img-three" class="sub-image" src="${picData.hits[3].webformatURL}"/>
+                                    </div>`;
+            document.getElementById("picture").innerHTML = pictureHtml;
+            document.getElementById("sub-pic").innerHTML = subpic;
+            console.log('Data received from pixabay:', picData);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+
+    let weatherHtml = '';
+    let colors = ['red', 'green', 'yellow', 'blue']
+    receivedWeatherInJson.data.forEach((element, index) => {
+        weatherHtml += `<div class="col-large-6">
+        <div class="weather-${colors[index]}">
+            <div class="weather-card-body">
+                <div class="card-title">
+                    <h2>${element.weather.description}</h2>
+                </div>
+                <div class="card-content">
+                    <h1 id="temperature">${element.temp}</h1>
+                    <h6>High => ${element.high_temp} Low => ${element.high_temp} </h6>
+                </div>
+                <div class="weather-card-icon">
+                    <img src="../src/client/assets/${element.weather.icon}.png">
+                    <h6>${element.valid_date}</h6>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    });
+    document.getElementById("weather").innerHTML = weatherHtml;
+};
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -7,13 +7,14 @@ const fetch = require('node-fetch');
 const app = express()
 var bodyParser = require('body-parser')
 const dotenv = require('dotenv');
-
+const geoApiUrl='http://api.geonames.org/searchJSON';
+const weatherApiUrl='https://api.weatherbit.io/v2.0/forecast/daily'
+const pixabayApiUrl='https://pixabay.com/api/';
 app.use(cors());
 app.use(express.static('dist'))
 dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -27,13 +28,21 @@ app.listen(8081, function () {
  app.get('/test', function (req, res) {
      res.send(mockAPIResponse)
  })
- console.log(`Your API key is ${process.env.API_KEY}`);
- 
- // POST Route
-app.post('/api', async function(req, res) {
-    const response = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=en&url=${req.body.url}`)
+ console.log(`Your API key is ${process.env.API_KEY}`); 
+  
+app.post('/geo', async function(req, res) {
+    console.log("datanb");
+    const response = await fetch(`${geoApiUrl}?q=${req.body.city}&maxRows=1&username=${process.env.GEO_USER}`)
     const data = await response.json()
-    console.log('response=>'+data)
     res.send(data)
 })
-  
+app.post('/weather', async function(req, res) {
+    const response = await fetch(`${weatherApiUrl}?lat=${req.body.lat}&lon=${req.body.lng}&days=4&key=${process.env.WEA_KEY}`)
+    const data = await response.json()
+    res.send(data)
+})
+app.post('/picture', async function(req,res){
+    const response = await fetch(`${pixabayApiUrl}?key=${process.env.PIX_KEY}&q=${req.body.city}&image_type=photo&pretty=true&orientation=horizontal&editors_choice=true&per_page=4&category=travel`)
+    const data = await response.json()
+    res.send(data)
+})
